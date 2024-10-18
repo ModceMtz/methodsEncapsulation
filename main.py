f6b548cd7eda0d2e0fb9c47ee93401ece11df4e2
ModceMtz <modcemartinez158@gmail.com>
2024-10-17 19:52:49 -0600

    Inicio

---
+++ main.py
@@ -0,0 +1,11 @@
+from classWindow import window
+
+ventana = window()
+ventana.setDimension(500, 500 )
+ventana.setTitulo("Mi primera vez")
+ventana.setResize(True)
+#print(ventana.getDimension())
+#print(ventana.getResize())
+#print(ventana.getTitulo())
+
+ventana.crear_ventana()
